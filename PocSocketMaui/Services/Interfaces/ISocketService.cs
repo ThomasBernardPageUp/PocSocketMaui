a4f1d1d3a6bbbc0d39ab8bcb4dc0a35622839117
Thomas <90254070+ThomasBernardPageUp@users.noreply.github.com>
2022-06-27 12:01:42 +0200

    Add comments

--- PocSocketMaui/Services/Interfaces/ISocketService.cs
+++ PocSocketMaui/Services/Interfaces/ISocketService.cs
@@ -6,9 +6,33 @@ namespace PocSocketMaui.Services.Interfaces;
 
 public interface ISocketService
 {
+	/// <summary>
+	/// History of all messages (sended and received)
+	/// </summary>
 	public ReadOnlyObservableCollection<MessageWrapper> Messages { get; }
+
+	/// <summary>
+	/// Connect to server
+	/// </summary>
+	/// <returns></returns>
 	Task ConnectToServerAsync();
+
+	/// <summary>
+	/// Disconnect to the server
+	/// </summary>
+	/// <returns></returns>
 	Task DisconnectToServerAsync();
+
+	/// <summary>
+	/// Read message
+	/// </summary>
+	/// <returns></returns>
 	Task ReadMessageAsync();
+
+	/// <summary>
+	/// Send a message to the server and add it in history list
+	/// </summary>
+	/// <param name="message"></param>
+	/// <returns></returns>
 	Task<bool> SendMessageAsync(string message);
 }
